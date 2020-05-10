6f2b4996762a0ba58eeab6733ed0729845c48ff5
Prahalad Belavadi <test@ALIPL0232.local>
2020-05-10 15:09:52 +0530

    dynamic routes: implement static props; Error: Can't resolve fs

---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Layout from '../../components/layouts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
